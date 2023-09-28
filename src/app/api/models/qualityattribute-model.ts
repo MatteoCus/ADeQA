a03e6afbcf44f32bf6bd3b6e4dae097368ab6fc8
MatteoCus <matteo.cusin58@gmail.com>
2023-09-28 15:42:46 +0200

    Added OpenAPI-generated services, models and functions

---
+++ src/app/api/models/qualityattribute-model.ts
@@ -0,0 +1,16 @@
+/* tslint:disable */
+/* eslint-disable */
+export interface QualityattributeModel {
+  ad_reference_id?: number;
+  attributedescription?: string;
+  attributename?: string;
+  attributeseqno?: number;
+  attributevalue?: string;
+  attributevaluetype?: 'Y' | 'N';
+  c_project_attribute_group_id?: number;
+  groupdescription?: string;
+  groupname?: string;
+  m_product_category_id?: number;
+  m_product_id?: number;
+  optionvalue?: string;
+}
