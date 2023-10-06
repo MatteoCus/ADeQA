b7144f552e96c89110c5f863500683a5cbe7b863
MatteoCus <matteo.cusin58@gmail.com>
2023-10-06 16:11:39 +0200

    Skeleton loading - table

--- src/app/components/log-modifier/log-modifier.component.ts
+++ src/app/components/log-modifier/log-modifier.component.ts
@@ -5,6 +5,9 @@ import { QualityAttributeService, QualitySaveLogService } from 'src/app/api/serv
 import { ActivePhaseService } from 'src/app/services/active-phase/active-phase.service';
 import { AuthInformationsService } from 'src/app/services/auth-informations/auth-informations.service';
 
+/**
+ * Classe che gestisce gli attributi relativi a una determinata fase selezionata ed i loro valori
+ */
 @Component({
   selector: 'app-log-modifier',
   templateUrl: './log-modifier.component.html',
@@ -12,30 +15,58 @@ import { AuthInformationsService } from 'src/app/services/auth-informations/auth
 })
 export class LogModifierComponent implements OnInit {
 
+  /**
+   * 
+   */
   public activeLog : string [] = [];
 
+  /**
+   * Le colonne da mostrare (la descrizione degli attributi che caratterizzano un controllo qualità per la fase selezionata)
+   */
   public displayedColumns: string[] = [];
 
+  /**
+   * Attributo booleano per gestire lo "skeleton loading"
+   */
   public loading: boolean = false;
 
+  /**
+   * Gli attributi che caratterizzano un controllo qualità per la fase selezionata
+   */
   public activeAttributes: Array<QualityattributeModel> = new Array<QualityattributeModel>();
 
-    /**
+  /**
    * Metodo per l'apertura della barra di visualizzazione di messaggi di stato
    * @param message Messaggio da mostrare
    * @param type Etichetta del pulsante di chiusura
   */
-    private openSnackBar(message: string, type: string): void { 
-      this.snackBar.open(message, type, {
-        panelClass: ['red-snackbar','login-snackbar'],
-        });
-    }
+  private openSnackBar(message: string, type: string): void { 
+    this.snackBar.open(message, type, {
+      panelClass: ['red-snackbar','login-snackbar'],
+      });
+  }
 
+  /**
+   * Costruttore della classe che gestisce gli attributi relativi a una fase selezionata ed i loro valori
+   * @param activePhaseService Servizio che gestisce la fase attualmente selezionata
+   * @param qualityAttributeService Servizio per ottenere gli attributi per la fase selezionata
+   * @param qualitySaveLogService Servizio per eseguire operazioni CRUD relativamente a log di controllo qualità
+   * @param authInfoService Servizio di gestione delle informazioni di autenticazione utente
+   * @param snackBar Barra di notifica eventi
+   */
   constructor(private activePhaseService: ActivePhaseService, private qualityAttributeService: QualityAttributeService, private qualitySaveLogService: QualitySaveLogService, private authInfoService: AuthInformationsService, private snackBar: MatSnackBar){}
 
+  /**
+   * Sottoscrizione allo stream delle fasi selezionate
+   */
   ngOnInit(): void {
     this.activePhaseService.getActivePhase().subscribe(activePhase => this.fetchAttributes(activePhase));
   }
+
+  /**
+   * Metodo per ottenere gli attributi relativi alla fase attualmente attiva
+   * @param activePhase Fase attualmente attiva
+   */
   private fetchAttributes(activePhase: QualityphaseModel): void {
     this.loading = true;
 
