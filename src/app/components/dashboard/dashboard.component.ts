383c087c43476961f0255de9c6adbfea9b328e48
MatteoCus <matteo.cusin58@gmail.com>
2023-10-12 11:07:46 +0200

    Resized UI for tablet

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { ChangeDetectorRef, Component, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthInformationsService } from 'src/app/services/auth-informations/auth-informations.service';
 
@@ -10,17 +11,29 @@ import { AuthInformationsService } from 'src/app/services/auth-informations/auth
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnDestroy {
+
+  private _mobileQueryListener: () => void;
+
+  public mobileQuery: MediaQueryList;
 
   /**
    * Costruttore della classe che gestisce la vista principale, reindirizza al login con username in assenza delle informazioni di autenticazione necessarie (token, userId)
    * @param authInfoService Servizio per gestire le informazioni relative all'autenticazione 
    * @param router Router per eseguire dei reindirizzamenti su browser
    */
-  constructor(private authInfoService: AuthInformationsService, private router: Router) {
+  constructor(private authInfoService: AuthInformationsService, private router: Router, changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
     if(this.authInfoService.Token == "" || this.authInfoService.UserId as any as number == 0){
       this.router.navigate(['login/username']);
     }
+
+    this.mobileQuery = media.matchMedia('(max-width: 1200px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
   }
 
 }
