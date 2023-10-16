61651d45f6e7d8a87fae1987bfeeab553a9f78f1
MatteoCus <matteo.cusin58@gmail.com>
2023-10-16 13:00:04 +0200

    Improved translation, modified manifest

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,8 +12,8 @@ export class AppComponent {
    * Costruttore della classe di entry point
    * @param translate Servizio di traduzione di ngx-translate
    */
-  constructor(translate: TranslateService) {
-    translate.setDefaultLang('it');
-    translate.use(localStorage.getItem('lang') || 'it');
-  }
+  constructor(private translateService: TranslateService) { 
+    this.translateService.setDefaultLang('it');
+    this.translateService.use(localStorage.getItem('lang') || 'it');
+   }
 }
