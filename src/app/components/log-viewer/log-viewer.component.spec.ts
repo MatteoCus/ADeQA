c7fd9b95e17240eef5eacd0e633fda00da344eea
MatteoCus <matteo.cusin58@gmail.com>
2023-10-20 10:06:25 +0200

    Designed new custom logo

--- src/app/components/log-viewer/log-viewer.component.spec.ts
+++ src/app/components/log-viewer/log-viewer.component.spec.ts
@@ -1,18 +1,33 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LogViewerComponent } from './log-viewer.component';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { TranslateCompiler, TranslateLoader, TranslateModule, TranslateParser, TranslateService, TranslateStore } from '@ngx-translate/core';
 
 describe('LogViewerComponent', () => {
   let component: LogViewerComponent;
   let fixture: ComponentFixture<LogViewerComponent>;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [LogViewerComponent]
+      declarations: [LogViewerComponent],
+      imports: [HttpClientTestingModule, TranslateModule],
+      providers: [
+        MatSnackBar,
+        TranslateService,
+        TranslateStore,
+        TranslateLoader,
+        TranslateCompiler,
+        TranslateModule,
+        TranslateParser
+      ]
     });
     fixture = TestBed.createComponent(LogViewerComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   it('should create', () => {
