7bf850bf1da8c74c7f4fd34000a7ae89f8ca9a11
MatteoCus <matteo.cusin58@gmail.com>
2023-10-24 17:26:27 +0200

    Tested confirm and logout dialog

--- src/app/components/confirm-data-dialog/confirm-data-dialog.component.spec.ts
+++ src/app/components/confirm-data-dialog/confirm-data-dialog.component.spec.ts
@@ -7,21 +7,77 @@ import { TranslateModule } from '@ngx-translate/core';
 describe('ConfirmDataDialogComponent', () => {
   let component: ConfirmDataDialogComponent;
   let fixture: ComponentFixture<ConfirmDataDialogComponent>;
+  let dialogRef: MatDialogRef<any, any>;
+
+  const dialogData = {
+    title: 'Test Title',
+    description: 'Test Description',
+    resume: ['Item 1', 'Item 2', 'Item 3'],
+  };
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [TranslateModule.forRoot()],
       declarations: [ConfirmDataDialogComponent],
       providers: [
-        { provide: MatDialogRef, useValue: {} }, { provide: MAT_DIALOG_DATA, useValue: { title: "Titolo", description: "Descrizione" } }
+        { provide: MatDialogRef, useValue: { close: () => {} }}, { provide: MAT_DIALOG_DATA, useValue: dialogData }
       ]
     });
     fixture = TestBed.createComponent(ConfirmDataDialogComponent);
     component = fixture.componentInstance;
+    dialogRef = TestBed.inject(MatDialogRef);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should initialize fromPage with dialog data', () => {
+    expect(component.fromPage).toEqual(dialogData);
+  });
+
+  it('should close the dialog with "confirm-option" event when confirm() is called', () => {
+    const dialogRefSpy = spyOn(dialogRef, 'close');
+    component.confirm();
+    expect(dialogRefSpy).toHaveBeenCalledWith({ event: 'confirm-option' });
+  });
+
+  it('should close the dialog with "cancel-option" event when cancel() is called', () => {
+    const dialogRefSpy = spyOn(dialogRef, 'close');
+    component.cancel();
+    expect(dialogRefSpy).toHaveBeenCalledWith({ event: 'cancel-option' });
+  });
+
+  it('should render the title and description from dialog data', () => {
+    const titleElement = fixture.nativeElement.querySelector('#title');
+    expect(titleElement.textContent.trim()).toBe(dialogData.title);
+
+    const descriptionElement = fixture.nativeElement.querySelector('p:not(#title)');
+    expect(descriptionElement.textContent.trim()).toBe(dialogData.description);
+  });
+
+  it('should render the list items from dialog data', () => {
+    const listItems = fixture.nativeElement.querySelectorAll('li');
+    expect(listItems.length).toBe(dialogData.resume.length);
+
+    listItems.forEach((item: { textContent: string; }, index: number) => {
+      expect(item.textContent.trim()).toBe(dialogData.resume[index]);
+    });
+  });
+
+  it('should call confirm() when the "Conferma" button is clicked', () => {
+    const confirmButton = fixture.nativeElement.querySelector('button[color="primary"]');
+    spyOn(component, 'confirm');
+    confirmButton.click();
+    expect(component.confirm).toHaveBeenCalled();
+  });
+
+  it('should call cancel() when the "Annulla" button is clicked', () => {
+    const cancelButton = fixture.nativeElement.querySelector('button[color="accent"]');
+    spyOn(component, 'cancel');
+    cancelButton.click();
+    expect(component.cancel).toHaveBeenCalled();
+  });
+
 });
--- src/app/components/logout-dialog/logout-dialog.component.spec.ts
+++ src/app/components/logout-dialog/logout-dialog.component.spec.ts
@@ -7,6 +7,7 @@ import { TranslateModule } from '@ngx-translate/core';
 describe('LogoutDialogComponent', () => {
   let component: LogoutDialogComponent;
   let fixture: ComponentFixture<LogoutDialogComponent>;
+  let dialogRef: MatDialogRef<any, any>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -15,14 +16,53 @@ describe('LogoutDialogComponent', () => {
         MatDialogModule,
         TranslateModule.forRoot()
       ],
-      providers: [{ provide: MatDialogRef, useValue: {} }, { provide: MAT_DIALOG_DATA, useValue: { title: "Titolo", description: "Descrizione" } }]
+      providers: [{ provide: MatDialogRef, useValue: { close: () => {} }}, { provide: MAT_DIALOG_DATA, useValue: { title: "Titolo", description: "Descrizione" } }]
     });
     fixture = TestBed.createComponent(LogoutDialogComponent);
     component = fixture.componentInstance;
+    dialogRef = TestBed.inject(MatDialogRef);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should initialize fromPage with dialog data', () => {
+    expect(component.fromPage).toEqual({ title: "Titolo", description: "Descrizione" });
+  });
+
+  it('should close the dialog with "stay-option" event when stay() is called', () => {
+    const dialogRefSpy = spyOn(dialogRef, 'close')
+    component.stay();
+    expect(dialogRefSpy).toHaveBeenCalledWith({ event: 'stay-option' });
+  });
+
+  it('should close the dialog with "exit-option" event when exit() is called', () => {
+    const dialogRefSpy = spyOn(dialogRef, 'close')
+    component.exit();
+    expect(dialogRefSpy).toHaveBeenCalledWith({ event: 'exit-option' });
+  });
+
+  it('should render the title and description from dialog data', () => {
+    const titleElement = fixture.nativeElement.querySelector('#title');
+    expect(titleElement.textContent.trim()).toBe("Titolo");
+
+    const descriptionElement = fixture.nativeElement.querySelector('p:not(#title)');
+    expect(descriptionElement.textContent.trim()).toBe("Descrizione");
+  });
+
+  it('should call stay() when the "Rimani" button is clicked', () => {
+    const stayButton = fixture.nativeElement.querySelector('button[color="primary"]');
+    spyOn(component, 'stay');
+    stayButton.click();
+    expect(component.stay).toHaveBeenCalled();
+  });
+
+  it('should call exit() when the "Esci" button is clicked', () => {
+    const exitButton = fixture.nativeElement.querySelector('button[color="accent"]');
+    spyOn(component, 'exit');
+    exitButton.click();
+    expect(component.exit).toHaveBeenCalled();
   });
 });
